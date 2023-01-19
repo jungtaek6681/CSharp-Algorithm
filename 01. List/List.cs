c1f5c178e205658106d2fe7dd4c7af039329d025
jungtaek kim <kgajungtaek@gmail.com>
2023-01-19 20:59:38 +0900

    feat : List
    List : 런타임 중 크기를 확장할 수 있는 배열기반의 자료구조

---
+++ 01. List/List.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+	internal class List<T>
+	{
+		private const int DefaultCapacity = 4;
+
+		private T[] items;
+		private int size;
+
+		public List()
+		{
+			items = new T[DefaultCapacity];
+			size = 0;
+		}
+
+		public int Capacity { get { return items.Length; } }
+		public int Count { get { return size; } }
+
+		public T this[int index]
+		{
+			get
+			{
+				if (index < 0 || index >= size)
+					throw new IndexOutOfRangeException();
+
+				return items[index];
+			}
+			set
+			{
+				if (index < 0 || index >= size)
+					throw new IndexOutOfRangeException();
+
+				items[index] = value;
+			}
+		}
+
+		public void Add(T item)
+		{
+			if (size < items.Length)
+			{
+				items[size++] = item;
+			}
+			else
+			{
+				Grow();
+			}
+			
+		}
+
+		public void Clear()
+		{
+			items = new T[DefaultCapacity];
+			size = 0;
+		}
+
+		public T? Find(Predicate<T> match)
+		{
+			if (match == null) throw new ArgumentNullException();
+
+			for (int i = 0; i < size; i++)
+			{
+				if (match(items[i]))
+					return items[i];
+			}
+
+			return default(T);
+		}
+
+		public int FindIndex(Predicate<T> match)
+		{
+			return FindIndex(0, size, match);
+		}
+
+		public int FindIndex(int startIndex, int count, Predicate<T> match)
+		{
+			if (startIndex > size)
+				throw new ArgumentOutOfRangeException();
+			if (count < 0 || startIndex > size - count)
+				throw new ArgumentOutOfRangeException();
+			if (match == null)
+				throw new ArgumentNullException();
+
+			int endIndex = startIndex + count;
+			for (int i = startIndex; i < endIndex; i++)
+			{
+				if (match(items[i])) return i;
+			}
+			return -1;
+		}
+
+		public int IndexOf(T item)
+		{
+			return Array.IndexOf(items, item, 0, size);
+		}
+
+		public bool Remove(T item)
+		{
+			int index = IndexOf(item);
+			if (index >= 0)
+			{
+				RemoveAt(index);
+				return true;
+			}
+			return false;
+		}
+
+		public void RemoveAt(int index)
+		{
+			if (index < 0 || index >= size)
+				throw new IndexOutOfRangeException();
+
+			size--;
+			Array.Copy(items, index + 1, items, index, size - index);
+		}
+
+		private void Grow()
+		{
+			int newCapacity = items.Length * 2;
+			T[] newItems = new T[newCapacity];
+			Array.Copy(items, 0, newItems, 0, size);
+			items = newItems;
+		}
+	}
+}
---
+++ 01. List/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._List
+{
+	internal class Program
+	{
+		/******************************************************
+		 * 배열 (Array)
+		 * 
+		 * 연속적인 메모리상에 동일한 타입의 요소를 일렬로 저장하는 자료구조
+		 * 초기화때 정한 크기가 소멸까지 유지됨
+		 * 배열의 요소는 인덱스를 사용하여 직접적으로 엑세스 가능
+		 ******************************************************/
+
+		// <배열의 사용>
+		void Array()
+		{
+			int[] intArray = new int[100];
+
+			// 인덱스를 통한 접근
+			intArray[0] = 10;
+			int value = intArray[0];
+		}
+
+		// <배열의 시간복잡도>
+		// 접근		탐색
+		// O(1)		O(N)
+
+
+		/******************************************************
+		 * 동적배열 (Dynamic Array)
+		 * 
+		 * 런타임 중 크기를 확장할 수 있는 배열기반의 자료구조
+		 * 배열요소의 갯수를 특정할 수 없는 경우 사용
+		 ******************************************************/
+
+		// <List의 사용>
+		void List()
+		{
+			List<string> list = new List<string>();
+
+			// 배열 요소 삽입
+			list.Add("0번 데이터");
+			list.Add("1번 데이터");
+			list.Add("2번 데이터");
+
+			// 배열 요소 삭제
+			list.Remove("1번 데이터");
+
+			// 배열 요소 접근
+			list[0] = "데이터0";
+			string value = list[0];
+
+			// 배열 요소 탐색
+			string? findValue = list.Find(x => x.Contains('2'));
+			int findIndex = list.FindIndex(x => x.Contains('0'));
+		}
+
+		// <List의 시간복잡도>
+		// 접근		탐색		삽입		삭제
+		// O(1)		O(n)	O(n)	O(n)
+
+		static void Main(string[] args)
+		{
+			DataStructure.List<string> list = new DataStructure.List<string>();
+
+			list.Add("1번 데이터");
+			list.Add("2번 데이터");
+			list.Add("3번 데이터");
+			list.Add("4번 데이터");
+			list.Add("5번 데이터");
+
+			string value;
+			value = list[0];
+			value = list[1];
+			value = list[2];
+			value = list[3];
+			value = list[4];
+
+			list[0] = "5번 데이터";
+			list[1] = "4번 데이터";
+			list[2] = "3번 데이터";
+			list[3] = "2번 데이터";
+			list[4] = "1번 데이터";
+
+			list.Remove("3번 데이터");
+			list.Remove("2번 데이터");
+
+			string? findValue = list.Find(x => x.Contains('4'));
+			int findIndex = list.FindIndex(x => x.Contains('1'));
+		}
+	}
+}
