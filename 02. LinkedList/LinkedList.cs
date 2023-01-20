32a445f475bb9bba79e80358809fb99e63a5fd0a
jungtaek kim <kgajungtaek@gmail.com>
2023-01-20 16:18:01 +0900

    feat : LinkedList
    LinkedList : 데이터를 포함하는 노드들을 연결식으로 만든 자료구조

---
+++ 02. LinkedList/LinkedList.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataStructure
+{
+	public class LinkedListNode<T>
+	{
+		internal LinkedList<T> list;
+		internal LinkedListNode<T> prev;
+		internal LinkedListNode<T> next;
+		private T item;
+
+		public LinkedListNode(T value)
+		{
+			this.list = null;
+			this.prev = null;
+			this.next = null;
+			this.item = value;
+		}
+
+		public LinkedListNode(LinkedList<T> list, T value)
+		{
+			this.list = list;
+			this.prev = null;
+			this.next = null;
+			this.item = value;
+		}
+
+		public LinkedListNode(LinkedList<T> list, LinkedListNode<T> prev, LinkedListNode<T> next, T value)
+		{
+			this.list = list;
+			this.prev = prev;
+			this.next = next;
+			this.item = value;
+		}
+
+		public LinkedList<T> List { get { return list; } }
+		public LinkedListNode<T> Prev { get { return prev; } }
+		public LinkedListNode<T> Next { get { return next; } }
+
+		public T Item { get { return item; } set { item = value; } }
+	}
+
+	public class LinkedList<T>
+	{
+		private LinkedListNode<T> head;
+		private LinkedListNode<T> tail;
+		private int count;
+
+		public LinkedList()
+		{
+			this.head = null;
+			this.tail = null;
+			count = 0;
+		}
+
+		public LinkedListNode<T> First { get { return head; } }
+		public LinkedListNode<T> Last { get { return tail; } }
+		public int Count { get { return count; } }
+
+		public LinkedListNode<T> AddBefore(LinkedListNode<T> node, T value)
+		{
+			ValidateNode(node);
+			LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+			InsertNodeBefore(node, newNode);
+			if (node == head)
+				head = newNode;
+			return newNode;
+		}
+
+		public LinkedListNode<T> AddAfter(LinkedListNode<T> node, T value)
+		{
+			ValidateNode(node);
+			LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+			InsertNodeAfter(node, newNode);
+			if (node == tail)
+				tail = newNode;
+			return newNode;
+		}
+
+		public LinkedListNode<T> AddFirst(T value)
+		{
+			LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+			if (head != null)
+			{
+				InsertNodeBefore(head, newNode);
+				head = newNode;
+			}
+			else
+			{
+				InsertNodeToEmptyList(newNode);
+			}
+			return newNode;
+		}
+
+		public LinkedListNode<T> AddLast(T value)
+		{
+			LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+			if (tail != null)
+			{
+				InsertNodeAfter(tail, newNode);
+				tail = newNode;
+			}
+			else
+			{
+				InsertNodeToEmptyList(newNode);
+			}
+			return newNode;
+		}
+
+		public void Clear()
+		{
+			head = null;
+			tail = null;
+			count = 0;
+		}
+
+		public bool Contains(T value)
+		{
+			return Find(value) != null;
+		}
+
+		public LinkedListNode<T> Find(T value)
+		{
+			LinkedListNode<T>? node = head;
+			EqualityComparer<T> c = EqualityComparer<T>.Default;
+			if (value != null)
+			{
+				while (node != null)
+				{
+					if (c.Equals(node.Item, value))
+						return node;
+					else
+						node = node.next;
+				}
+			}
+			else
+			{
+				while (node != null)
+				{
+					if (node.Item == null)
+						return node;
+					else
+						node = node.next;
+				}
+			}
+			return null;
+		}
+
+		public bool Remove(T value)
+		{
+			LinkedListNode<T>? node = Find(value);
+			if (node != null)
+			{
+				Remove(node);
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public void Remove(LinkedListNode<T> node)
+		{
+			ValidateNode(node);
+			if (head == node)
+				head = node.next;
+			if (tail == node)
+				tail = node.prev;
+			RemoveNode(node);
+		}
+
+		public void RemoveFirst()
+		{
+			if (head == null)
+				throw new InvalidOperationException();
+
+			LinkedListNode<T> headNode = head;
+			Remove(headNode);
+		}
+
+		public void RemoveLast()
+		{
+			if (tail == null)
+				throw new InvalidOperationException();
+
+			LinkedListNode<T> tailNode = tail;
+			Remove(tailNode);
+		}
+
+		private void ValidateNode(LinkedListNode<T> node)
+		{
+			if (node == null)
+				throw new ArgumentNullException();
+			if (node.list != this)
+				throw new InvalidOperationException();
+		}
+
+		private void InsertNodeBefore(LinkedListNode<T> node, LinkedListNode<T> newNode)
+		{
+			newNode.next = node;
+			newNode.prev = node.prev;
+			if (newNode.prev != null)
+				newNode.prev.next = newNode;
+
+			node.prev = newNode;
+
+			count++;
+		}
+
+		private void InsertNodeAfter(LinkedListNode<T> node, LinkedListNode<T> newNode)
+		{
+			newNode.prev = node;
+			newNode.next = node.next;
+			if (newNode.next != null)
+				newNode.next.prev = newNode;
+
+			node.next = newNode;
+
+			count++;
+		}
+
+		private void InsertNodeToEmptyList(LinkedListNode<T> newNode)
+		{
+			if (count != 0)
+				throw new InvalidOperationException();
+
+			head = newNode;
+			tail = newNode;
+			count++;
+		}
+
+		private void RemoveNode(LinkedListNode<T> node)
+		{
+			if (node.list != this)
+				throw new InvalidOperationException();
+
+			if (node.prev != null)
+				node.prev.next = node.next;
+			if (node.next != null)
+				node.next.prev = node.prev;
+
+			count--;
+		}
+	}
+}
---
+++ 02. LinkedList/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._LinkedList
+{
+	internal class Program
+	{
+		/******************************************************
+		 * 연결리스트 (Linked List)
+		 * 
+		 * 데이터를 포함하는 노드들을 연결식으로 만든 자료구조
+		 * 노드는 데이터와 이전/다음 노드 객체를 참조하고 있음
+		 * 노드가 메모리에 연속적으로 배치되지 않고 이전/다음노드의 위치를 확인
+		 ******************************************************/
+
+		// <링크드리스트 사용>
+		void LinkedList()
+		{
+			LinkedList<string> linkedList = new LinkedList<string>();
+
+			// 링크드리스트 요소 삽입
+			linkedList.AddFirst("0번 앞데이터");
+			linkedList.AddFirst("1번 앞데이터");
+			linkedList.AddLast("0번 뒤데이터");
+			linkedList.AddLast("1번 뒤데이터");
+
+			// 링크드리스트 요소 삭제
+			linkedList.Remove("1번 앞데이터");
+
+			// 링크드리스트 요소 탐색
+			LinkedListNode<string> findNode = linkedList.Find("0번 뒤데이터");
+
+			// 링크드리스트 노드를 통한 노드 참조
+			LinkedListNode<string> prevNode = findNode.Previous;
+			LinkedListNode<string> nextNode = findNode.Next;
+
+			// 링크드리스트 노드를 통한 노드 삽입
+			linkedList.AddBefore(findNode, "찾은노드 앞데이터");
+			linkedList.AddAfter(findNode, "찾은노드 뒤데이터");
+
+			// 링크드리스트 노드를 통한 삭제
+			linkedList.Remove(findNode);
+		}
+
+		// <LinkedList의 시간복잡도>
+		// 접근		탐색		삽입		삭제
+		// O(1)		O(n)	O(n)	O(n)
+
+		static void Main(string[] args)
+		{
+			DataStructure.LinkedList<string> linkedList = new DataStructure.LinkedList<string>();
+
+			// 링크드리스트 요소 삽입
+			linkedList.AddFirst("0번 앞데이터");
+			linkedList.AddFirst("1번 앞데이터");
+			linkedList.AddLast("0번 뒤데이터");
+			linkedList.AddLast("1번 뒤데이터");
+
+			// 링크드리스트 요소 삭제
+			linkedList.Remove("1번 앞데이터");
+
+			// 링크드리스트 요소 탐색
+			DataStructure.LinkedListNode<string> findNode = linkedList.Find("0번 뒤데이터");
+
+			// 링크드리스트 노드를 통한 노드 참조
+			DataStructure.LinkedListNode<string> prevNode = findNode.Prev;
+			DataStructure.LinkedListNode<string> nextNode = findNode.Next;
+
+			// 링크드리스트 노드를 통한 노드 삽입
+			linkedList.AddBefore(findNode, "찾은노드 앞데이터");
+			linkedList.AddAfter(findNode, "찾은노드 뒤데이터");
+
+			// 링크드리스트 노드를 통한 삭제
+			linkedList.Remove(findNode);
+		}
+	}
+}
