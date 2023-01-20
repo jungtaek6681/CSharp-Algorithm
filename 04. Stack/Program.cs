343826752c8cf1ca2f64731c79276c2fa572012f
jungtaek kim <kgajungtaek@gmail.com>
2023-01-20 21:12:59 +0900

    feat : Stack
    Stack : 선입후출(FILO), 후입선출(LIFO) 방식의 자료구조

---
+++ 04. Stack/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Stack
+{
+	internal class Program
+	{
+		/******************************************************
+		 * 스택 (Stack)
+		 * 
+		 * 선입후출(FILO), 후입선출(LIFO) 방식의 자료구조
+		 * 가장 최신 입력된 순서대로 처리해야 하는 상황에 이용
+		 ******************************************************/
+
+		void Stack()
+		{
+			Stack<int> stack = new Stack<int>();
+
+			for (int i = 0; i < 5; i++) stack.Push(i);  // 입력순서 : 0 1 2 3 4
+
+			Console.WriteLine(stack.Peek());			// 최상단 : 4
+
+			while (stack.Count > 0)
+			{
+				Console.WriteLine(stack.Pop());         // 출력순서 : 4 3 2 1 0
+			}
+		}
+
+
+		static void Main(string[] args)
+		{
+			DataStructure.Stack<int> stack = new DataStructure.Stack<int>();
+
+			for (int i = 0; i < 5; i++) stack.Push(i);  // 입력순서 : 0 1 2 3 4
+
+			Console.WriteLine(stack.Peek());            // 최상단 : 4
+
+			while (stack.Count > 0)
+			{
+				Console.WriteLine(stack.Pop());         // 출력순서 : 4 3 2 1 0
+			}
+		}
+	}
+}
---
+++ 04. Stack/Stack.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+	public class Stack<T>
+	{
+		private const int DefaultCapacity = 4;
+
+		private T[] array;
+		private int topIndex;
+
+		public Stack()
+		{
+			array = new T[DefaultCapacity];
+			topIndex = -1;
+		}
+
+		public int Count { get { return topIndex + 1; } }
+
+		public void Clear()
+		{
+			array = new T[DefaultCapacity];
+			topIndex = -1;
+		}
+
+		public T Peek()
+		{
+			if (IsEmpty())
+				throw new InvalidOperationException();
+
+			return array[topIndex];
+		}
+
+		public bool TryPeek(out T result)
+		{
+			if (IsEmpty())
+			{
+				result = default(T);
+				return false;
+			}
+			else
+			{
+				result = array[topIndex];
+				return true;
+			}
+		}
+
+		public T Pop()
+		{
+			if (IsEmpty())
+				throw new InvalidOperationException();
+
+			return array[topIndex--];
+		}
+
+		public bool TryPop(out T result)
+		{
+			if (IsEmpty())
+			{
+				result = default(T);
+				return false;
+			}
+			else
+			{
+				result = array[topIndex--];
+				return true;
+			}
+		}
+
+		public void Push(T item)
+		{
+			if (IsFull())
+			{
+				Grow();
+			}
+			array[++topIndex] = item;
+		}
+
+		private void Grow()
+		{
+			int newCapacity = array.Length * 2;
+			T[] newArray = new T[newCapacity];
+			Array.Copy(array, 0, newArray, 0, Count);
+			array = newArray;
+		}
+
+		private bool IsEmpty()
+		{
+			return Count == 0;
+		}
+
+		private bool IsFull()
+		{
+			return Count == array.Length;
+		}
+	}
+}
