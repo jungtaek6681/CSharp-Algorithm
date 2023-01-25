290b9e071c86afe0d5705d8a0f642b174895744a
jungtaek kim <kgajungtaek@gmail.com>
2023-01-25 16:57:31 +0900

    feat : Heap
    Heap : 부모 노드가 항상 자식노드보다 우선순위가 높은 속성을 만족하는 트리기반의 자료구조
    많은 자료 중 우선순위가 가장 높은 요소를 빠르게 가져오기 위해 사용

---
+++ 06. Heap/PriorityQueue.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataStructure
+{
+	internal class PriorityQueue<TElement, TPriority>
+	{
+		private struct Node
+		{
+			public TElement Element;
+			public TPriority Priority;
+		}
+
+		private List<Node> nodes;
+		private IComparer<TPriority> comparer;
+
+		public PriorityQueue()
+		{
+			this.nodes = new List<Node>();
+			this.comparer = Comparer<TPriority>.Default;
+		}
+
+		public PriorityQueue(IComparer<TPriority> comparer)
+		{
+			this.nodes = new List<Node>();
+			this.comparer = comparer;
+		}
+
+		public int Count { get { return nodes.Count; } }
+		public IComparer<TPriority> Comparer { get { return comparer; } }
+
+		public void Enqueue(TElement element, TPriority priority)
+		{
+			Node newNode = new Node() { Element = element, Priority = priority };
+
+			PushHeap(newNode);
+		}
+
+		public TElement Peek()
+		{
+			if (nodes.Count == 0)
+				throw new InvalidOperationException();
+
+			return nodes[0].Element;
+		}
+
+		public bool TryPeek(out TElement element, out TPriority priority)
+		{
+			if (nodes.Count == 0)
+			{
+				element = default(TElement);
+				priority = default(TPriority);
+				return false;
+			}
+
+			element = nodes[0].Element;
+			priority = nodes[0].Priority;
+			return true;
+		}
+
+		public TElement Dequeue()
+		{
+			if (nodes.Count == 0)
+				throw new InvalidOperationException();
+
+			Node rootNode = nodes[0];
+			PopHeap();
+			return rootNode.Element;
+		}
+
+		public bool TryDequeue(out TElement element, out TPriority priority)
+		{
+			if (nodes.Count == 0)
+			{
+				element = default(TElement);
+				priority = default(TPriority);
+				return false;
+			}
+
+			Node rootNode = nodes[0];
+			element = rootNode.Element;
+			priority = rootNode.Priority;
+			PopHeap();
+			return true;
+		}
+
+		private void PushHeap(Node newNode)
+		{
+			nodes.Add(newNode);
+			int newNodeIndex = nodes.Count - 1;
+			while (newNodeIndex > 0)
+			{
+				int parentIndex = GetParentIndex(newNodeIndex);
+				Node parentNode = nodes[parentIndex];
+
+				if (comparer.Compare(newNode.Priority, parentNode.Priority) < 0)
+				{
+					nodes[newNodeIndex] = parentNode;
+					newNodeIndex = parentIndex;
+				}
+				else
+				{
+					break;
+				}
+			}
+			nodes[newNodeIndex] = newNode;
+		}
+
+		private void PopHeap()
+		{
+			Node lastNode = nodes[nodes.Count - 1];
+			nodes.RemoveAt(nodes.Count - 1);
+
+			int index = 0;
+			while (index < nodes.Count)
+			{
+				int leftChildIndex = GetLeftChildIndex(index);
+				int rightChildIndex = GetRightChildIndex(index);
+
+				if (rightChildIndex < nodes.Count)
+				{
+					int compareIndex = comparer.Compare(nodes[leftChildIndex].Priority, nodes[rightChildIndex].Priority) < 0 ?
+					leftChildIndex : rightChildIndex;
+
+					if (comparer.Compare(nodes[compareIndex].Priority, lastNode.Priority) < 0)
+					{
+						nodes[index] = nodes[compareIndex];
+						index = compareIndex;
+					}
+					else
+					{
+						nodes[index] = lastNode;
+						break;
+					}
+				}
+				else if (leftChildIndex < nodes.Count)
+				{
+					if (comparer.Compare(nodes[leftChildIndex].Priority, lastNode.Priority) < 0)
+					{
+						nodes[index] = nodes[leftChildIndex];
+						index = leftChildIndex;
+					}
+					else
+					{
+						nodes[index] = lastNode;
+						break;
+					}
+				}
+				else
+				{
+					nodes[index] = lastNode;
+					break;
+				}
+			}
+		}
+
+		private int GetParentIndex(int childIndex)
+		{
+			return (childIndex - 1) / 2;
+		}
+
+		private int GetLeftChildIndex(int parentIndex)
+		{
+			return parentIndex * 2 + 1;
+		}
+
+		private int GetRightChildIndex(int parentIndex)
+		{
+			return parentIndex * 2 + 2;
+		}
+	}
+}
+
+
+
+/*private void PopHeap()
+{
+	Node lastNode = nodes[nodes.Count - 1];
+	nodes.RemoveAt(nodes.Count - 1);
+
+	int index = 0;
+	while (index < nodes.Count)
+	{
+		int leftChildIndex = GetLeftChildIndex(index);
+		int rightChildIndex = GetRightChildIndex(index);
+
+		if (rightChildIndex < nodes.Count)
+		{
+			int compareIndex = comparer.Compare(nodes[leftChildIndex].Priority, nodes[rightChildIndex].Priority) < 0 ?
+			leftChildIndex : rightChildIndex;
+
+			if (comparer.Compare(nodes[compareIndex].Priority, lastNode.Priority) < 0)
+			{
+				nodes[index] = nodes[compareIndex];
+				index = compareIndex;
+			}
+			else
+			{
+				break;
+			}
+		}
+		else if (leftChildIndex < nodes.Count)
+		{
+			if (comparer.Compare(nodes[leftChildIndex].Priority, lastNode.Priority) < 0)
+			{
+				nodes[index] = nodes[leftChildIndex];
+				index = leftChildIndex;
+			}
+			else
+			{
+				break;
+			}
+		}
+		else
+		{
+			break;
+		}
+
+		if (comparer.Compare(lastNode.Priority, minChild.Priority) < 0)
+			break;
+
+
+	}
+	nodes[index] = lastNode;
+}*/
---
+++ 06. Heap/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Heap
+{
+	internal class Program
+	{
+		/******************************************************
+		 * 힙 (Heap)
+		 * 
+		 * 부모 노드가 항상 자식노드보다 우선순위가 높은 속성을 만족하는 트리기반의 자료구조
+		 * 많은 자료 중 우선순위가 가장 높은 요소를 빠르게 가져오기 위해 사용
+		 ******************************************************/
+
+		void PriorityQueue()
+		{
+			// 기본 int 우선순위(오름차순) 적용
+			PriorityQueue<string, int> pq1= new PriorityQueue<string, int>();
+
+			pq1.Enqueue("데이터1", 1);		// 우선순위와 데이터를 추가
+			pq1.Enqueue("데이터2", 3);
+			pq1.Enqueue("데이터3", 5);
+			pq1.Enqueue("데이터4", 2);
+			pq1.Enqueue("데이터5", 4);
+
+			while (pq1.Count > 0) Console.WriteLine(pq1.Dequeue());	// 우선순위가 높은 순서대로 데이터 출력
+
+
+			// 수정 int 우선순위 적용
+			PriorityQueue<string, int> pq2 = new PriorityQueue<string, int>(Comparer<int>.Create((a,b)=>b-a));
+
+			pq2.Enqueue("데이터1", 1);     // 우선순위와 데이터를 추가
+			pq2.Enqueue("데이터2", 3);
+			pq2.Enqueue("데이터3", 5);
+			pq2.Enqueue("데이터4", 2);
+			pq2.Enqueue("데이터5", 4);
+
+			while (pq2.Count > 0) Console.WriteLine(pq2.Dequeue()); // 우선순위가 높은 순서대로 데이터 출력
+		}
+
+		static void Main(string[] args)
+		{
+			// 기본 int 우선순위(오름차순) 적용
+			DataStructure.PriorityQueue<string, int> pq1 = new DataStructure.PriorityQueue<string, int>();
+
+			pq1.Enqueue("데이터1", 1);     // 우선순위와 데이터를 추가
+			pq1.Enqueue("데이터2", 3);
+			pq1.Enqueue("데이터3", 5);
+			pq1.Enqueue("데이터4", 2);
+			pq1.Enqueue("데이터5", 4);
+
+			while (pq1.Count > 0) Console.WriteLine(pq1.Dequeue()); // 우선순위가 높은 순서대로 데이터 출력
+
+
+			// 수정 int 우선순위 적용
+			DataStructure.PriorityQueue<string, int> pq2 = new DataStructure.PriorityQueue<string, int>(Comparer<int>.Create((a, b) => b - a));
+
+			pq2.Enqueue("데이터1", 1);     // 우선순위와 데이터를 추가
+			pq2.Enqueue("데이터2", 3);
+			pq2.Enqueue("데이터3", 5);
+			pq2.Enqueue("데이터4", 2);
+			pq2.Enqueue("데이터5", 4);
+
+			while (pq2.Count > 0) Console.WriteLine(pq2.Dequeue()); // 우선순위가 높은 순서대로 데이터 출력
+		}
+	}
+}
