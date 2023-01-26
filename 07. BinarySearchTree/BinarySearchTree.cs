a22b174b23664c625ed46f3b79a335c05ceb1544
jungtaek kim <kgajungtaek@gmail.com>
2023-01-26 18:20:06 +0900

    feat : BinarySearchTree
    BinarySearchTree : 이진속성과 탐색속성을 적용한 트리
    이진탐색을 통한 탐색영역을 절반으로 줄여가며 탐색 가능

---
+++ 07. BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+	internal class BinarySearchTree<T> where T : IComparable<T>
+	{
+		private Node root;
+
+		public BinarySearchTree()
+		{
+			this.root = null;
+		}
+		
+		public bool Add(T item)
+		{
+			Node newNode = new Node(item, null, null, null);
+
+			if (root == null)
+			{
+				root = newNode;
+				return true;
+			}
+
+			Node current = root;
+			while (current != null)
+			{
+				if (item.CompareTo(current.Item) < 0)
+				{
+					if (current.Left != null)
+					{
+						current = current.Left;
+					}
+					else
+					{
+						current.Left = newNode;
+						newNode.Parent = current;
+						break;
+					}
+				}
+				else if (item.CompareTo(current.Item) > 0)
+				{
+					if (current.Right != null)
+					{
+						current = current.Right;
+					}
+					else
+					{
+						current.Right = newNode;
+						newNode.Parent = current;
+						break;
+					}
+				}
+				else
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public bool Remove(T item)
+		{
+			if (root == null)
+				return false;
+
+			Node findNode = FindNode(item);
+			if (findNode == null)
+			{
+				return false;
+			}
+			else
+			{
+				EraseNode(findNode);
+				return true;
+			}
+		}
+
+		public void Clear()
+		{
+			root = null;
+		}
+
+		public bool TryGetValue(T item, out T outValue)
+		{
+			if (root == null)
+			{
+				outValue = default(T);
+				return false;
+			}
+
+			Node findNode = FindNode(item);
+			if (findNode == null)
+			{
+				outValue = default(T);
+				return false;
+			}
+			else
+			{
+				outValue = findNode.Item;
+				return true;
+			}
+		}
+
+		private Node FindNode(T item)
+		{
+			if (root == null)
+				return null;
+
+			Node current = root;
+			while (current != null)
+			{
+				if (item.CompareTo(current.Item) < 0)
+				{
+					current = current.Left;
+				}
+				else if (item.CompareTo(current.Item) > 0)
+				{
+					current = current.Right;
+				}
+				else
+				{
+					return current;
+				}
+			}
+
+			return null;
+		}
+
+		private void EraseNode(Node node)
+		{
+			if (node.HasNoChild)
+			{
+				if (node.IsLeftChild)
+					node.Parent.Left = null;
+				else if (node.IsRightChild)
+					node.Parent.Right = null;
+				else
+					root = null;
+			}
+			else if (node.HasLeftChild || node.HasRightChild)
+			{
+				Node parent = node.Parent;
+				Node child = node.HasLeftChild ? node.Left : node.Right;
+
+				if (node.IsLeftChild)
+				{
+					parent.Left = child;
+					child.Parent = parent;
+				}
+				else if (node.IsRightChild)
+				{
+					parent.Right = child;
+					child.Parent = parent;
+				}
+				else
+				{
+					root = child;
+					child.Parent = null;
+				}
+			}
+			else
+			{
+				Node nextNode = node.Right;
+				while (nextNode.Left != null)
+				{
+					nextNode = nextNode.Left;
+				}
+				node.Item = nextNode.Item;
+				EraseNode(nextNode);
+			}
+		}
+
+		private class Node
+		{
+			private T item;
+			private Node parent;
+			private Node left;
+			private Node right;
+
+			public Node(T item, Node parent, Node left, Node right)
+			{
+				this.item = item;
+				this.parent = parent;
+				this.left = left;
+				this.right = right;
+			}
+
+			public T Item { get { return item; } set { item = value; } }
+			public Node Parent { get { return parent; } set { parent = value; } }
+			public Node Left { get { return left; } set { left = value; } }
+			public Node Right { get { return right; } set { right = value; } }
+
+			public bool IsRootNode { get { return parent == null; } }
+			public bool IsLeftChild { get { return parent != null && parent.left == this; } }
+			public bool IsRightChild { get { return parent != null && parent.right == this; } }
+
+			public bool HasNoChild { get { return left == null && right == null; } }
+			public bool HasLeftChild { get { return left != null && right == null; } }
+			public bool HasRightChild { get { return left == null && right != null; } }
+			public bool HasBothChild { get { return left != null && right != null; } }
+		}
+	}
+}
---
+++ 07. BinarySearchTree/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace _07._BinarySearchTree
+{
+	internal class Program
+	{
+		/******************************************************
+		 * 트리 (Tree)
+		 * 
+		 * 계층적인 자료를 나타내는데 자주 사용되는 자료구조
+		 * 부모노드가 0개 이상의 자식노드들을 가질 수 있음
+		 * 한 노드에서 출발하여 다시 자기 자신의 노드로 돌아오는 순환구조를 가질 수 없음
+		 ******************************************************/
+
+		/******************************************************
+		 * 이진탐색트리 (BinarySearchTree)
+		 * 
+		 * 이진속성과 탐색속성을 적용한 트리
+		 * 이진탐색을 통한 탐색영역을 절반으로 줄여가며 탐색 가능
+		 * 이진 : 부모노드는 최대 2개의 자식노드들을 가질 수 있음
+		 * 탐색 : 자신의 노드보다 작은 값들은 왼쪽, 큰 값들은 오른쪽에 위치
+		 ******************************************************/
+
+		// <이진탐색트리의 시간복잡도>
+		// 접근			탐색			삽입			삭제
+		// O(log n)		O(log n)	O(log n)	O(log n)
+
+		// <이진탐색트리의 주의점>
+		// 이진탐색트리의 노드들이 한쪽 자식으로만 추가되는 불균형 발생 가능
+		// 이 경우 탐색영역이 절반으로 줄여지지 않기 때문에 시간복잡도 증가
+		// 이러한 현상을 막기 위해 자가균형기능을 추가한 트리의 사용이 일반적
+		// 대표적인 방식으로 Red-Black Tree, AVL Tree 등이 있음
+
+		void BinarySearchTree()
+		{
+			// value 이진탐색트리
+			SortedSet<int> sortedSet = new SortedSet<int>();
+
+			sortedSet.Add(1);
+			sortedSet.Add(2);
+			sortedSet.Add(3);
+			sortedSet.Add(4);
+			sortedSet.Add(5);
+
+			int searchValue1;
+			sortedSet.TryGetValue(3, out searchValue1);				// 탐색 시도
+			
+			// key, value 이진탐색트리
+			SortedDictionary<int, string> sortedDictionary = new SortedDictionary<int, string>();
+
+			sortedDictionary.Add(1, "a");
+			sortedDictionary.Add(2, "b");
+			sortedDictionary.Add(3, "c");
+			sortedDictionary.Add(4, "d");
+			sortedDictionary.Add(5, "e");
+
+			string searchValue2;
+			sortedDictionary.TryGetValue(3, out searchValue2);		// 탐색 시도
+			string indexerValue2 = sortedDictionary[3];				// 인덱서를 통한 탐색
+
+
+			// 이진탐색 검색효율
+			int[] array = new int[10000000];
+			SortedSet<int> set = new SortedSet<int>();
+
+			Random random = new Random();
+			int rand;
+			for (int i = 0; i < 1000000; i++)
+			{
+				rand = random.Next();
+				array[i] = rand;
+				set.Add(rand);
+			}
+			array[9999999] = -1;
+			set.Add(-1);
+
+			Stopwatch stopwatch = new Stopwatch();
+
+			stopwatch.Start();
+			Array.Find(array, (x) => x == -1);
+			stopwatch.Stop();
+			Console.WriteLine("배열 time : {0}", stopwatch.ElapsedTicks);
+
+			stopwatch.Restart();
+			int value;
+			set.TryGetValue(-1, out value);
+			stopwatch.Stop();
+			Console.WriteLine("트리 time : {0}", stopwatch.ElapsedTicks);
+		}
+
+		static void Main(string[] args)
+		{
+			DataStructure.BinarySearchTree<int> bst = new DataStructure.BinarySearchTree<int>();
+
+			bst.Add(3);
+			bst.Add(1);
+			bst.Add(5);
+			bst.Add(2);
+			bst.Add(4);
+			bst.Add(6);
+
+			bst.Remove(3);
+			bst.Remove(4);
+		}
+	}
+}
