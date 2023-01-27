55a603e20b3fcca14cedcdd5dac42e86df10bb3f
jungtaek kim <kgajungtaek@gmail.com>
2023-01-27 13:31:32 +0900

    feat : HashTable
    HashTable : 키 값을 해시함수로 해싱하여 해시테이블의 특정 위치로 직접 엑세스하도록 만든 방식

---
+++ 08. HashTable/HashTable.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+	internal class HashTable<TKey, TValue> where TKey : IEquatable<TKey>
+	{
+		private const int DefaultCapacity = 1000;
+
+		private struct Entry
+		{
+			public enum State { None, Using, Deleted }
+
+			public int hashCode;
+			public State state;
+			public TKey key;
+			public TValue value;
+		}
+
+		private Func<TKey, int> hashFunc;
+		private Entry[] table;
+
+		public HashTable()
+		{
+			table = new Entry[DefaultCapacity];
+			hashFunc = HashFunc;
+		}
+
+		public HashTable(Func<TKey, int> hashFunc)
+		{
+			this.table = new Entry[DefaultCapacity];
+			this.hashFunc = hashFunc;
+		}
+
+		public TValue this[TKey key]
+		{
+			get
+			{
+				TValue value;
+				if (TryGetValue(key, out value))
+					return value;
+				else
+					throw new KeyNotFoundException();
+			}
+			set
+			{
+				TryInsert(key, value, InsertionBehavior.OverrideExist);
+			}
+		}
+
+		public void Add(TKey key, TValue value)
+		{
+			TryInsert(key, value, InsertionBehavior.ThrowOnExisting);
+		}
+
+		public bool TryAdd(TKey key, TValue value)
+		{
+			return TryInsert(key, value, InsertionBehavior.None);
+		}
+
+		public void Clear()
+		{
+			table = new Entry[DefaultCapacity];
+		}
+
+		public bool ContainsKey(TKey key)
+		{
+			return TryGetValue(key, out var value);
+		}
+
+		public bool TryGetValue(TKey key, out TValue value)
+		{
+			int index = FindIndex(key);
+
+			if (index < 0)
+			{
+				value = default(TValue);
+				return false;
+			}
+			else
+			{
+				value = table[index].value;
+				return true;
+			}
+		}
+
+		public bool Remove(TKey key)
+		{
+			int index = FindIndex(key);
+
+			if (index < 0)
+			{
+				return false;
+			}
+			else
+			{
+				table[index].state = Entry.State.Deleted;
+				return true;
+			}
+		}
+
+		private int HashFunc(TKey key)
+		{
+			if (key == null)
+				throw new ArgumentNullException("key");
+
+			return key.GetHashCode();
+		}
+
+		private int DoubleHash(int index)
+		{
+			return ++index % table.Length;
+		}
+
+		private enum InsertionBehavior { None, OverrideExist, ThrowOnExisting }
+		private bool TryInsert(TKey key, TValue value, InsertionBehavior behavior)
+		{
+			int hashCode = hashFunc(key);
+			int index = Math.Abs(hashCode) % table.Length;
+			while (table[index].state == Entry.State.Using)
+			{
+				if (key.Equals(table[index].key))
+				{
+					switch (behavior)
+					{
+						case InsertionBehavior.OverrideExist:
+							table[index].hashCode = hashCode;
+							table[index].key = key;
+							table[index].value = value;
+							return true;
+						case InsertionBehavior.ThrowOnExisting:
+							throw new ArgumentException();
+						case InsertionBehavior.None:
+						default:
+							return false;
+					}
+				}
+				index = DoubleHash(index);
+			}
+
+			table[index].hashCode = hashCode;
+			table[index].state = Entry.State.Using;
+			table[index].key = key;
+			table[index].value = value;
+			return true;
+		}
+
+		private int FindIndex(TKey key)
+		{
+			int hashCode = hashFunc(key);
+			int index = Math.Abs(hashCode) % table.Length;
+			while (table[index].state == Entry.State.Using)
+			{
+				if (key.Equals(table[index].key))
+				{
+					return index;
+				}
+				index = DoubleHash(index);
+			}
+
+			return -1;
+		}
+	}
+}
---
+++ 08. HashTable/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Collections;
+
+namespace _08._HashTable
+{
+	internal class Program
+	{
+		/******************************************************
+		 * 해시테이블 (HashTable)
+		 * 
+		 * 키 값을 해시함수로 해싱하여 해시테이블의 특정 위치로 직접 엑세스하도록 만든 방식
+		 * 해시 : 임의의 길이를 가진 데이터를 고정된 길이를 가진 데이터로 매핑
+		 ******************************************************/
+
+		// <해시테이블의 시간복잡도>
+		// 접근			탐색			삽입			삭제
+		// X			O(1)		O(1)		O(1)
+
+		// <해시테이블 주의점 - 충돌>
+		// 해시함수가 서로 다른 입력 값에 대해 동일한 해시테이블 주소를 반환하는 것
+		// 모든 입력 값에 대해 고유한 해시 값을 만드는 것은 불가능하며 충돌은 피할 수 없음
+		// 대표적인 충돌 해결방안으로 체이닝과 개방주소법이 있음
+
+		// <충돌해결방안 - 체이닝>
+		// 해시 충돌이 발생하면 연결리스트로 데이터들을 연결하는 방식
+		// 장점 : 해시테이블에 자료가 많아지더라도 성능저하가 적음
+		// 단점 : 해시테이블 외 추가적인 저장공간이 필요
+
+		// <충돌해결방안 - 개방주소법>
+		// 해시 충돌이 발생하면 다른 빈 공간에 데이터를 삽입하는 방식
+		// 해시 충돌시 선형탐색, 제곱탐색, 이중해시 등을 통해 다른 빈 공간을 선정
+		// 장점 : 추가적인 저장공간이 필요하지 않음, 삽입삭제시 오버헤드가 적음
+		// 단점 : 해시테이블에 자료가 많아질수록 성능저하가 많음
+		// 해시테이블의 공간 사용률이 높을 경우 성능저하가 발생하므로 재해싱 과정을 진행함
+		// 재해싱 : 해시테이블의 크기를 늘리고 테이블 내의 모든 데이터를 다시 해싱 
+
+		void Dictionary()
+		{
+			Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+			dictionary.Add("txt", "텍스트 파일");
+			dictionary.Add("bmp", "이미지 파일");
+			dictionary.Add("mp3", "오디오 파일");
+
+			Console.WriteLine(dictionary["txt"]);       // 키값은 인덱서를 통해 접근
+
+			if (dictionary.ContainsKey("mp3"))
+				Console.WriteLine("mp3 키 값의 데이터가 있음");
+			else
+				Console.WriteLine("mp3 키 값의 데이터가 없음");
+
+			if (dictionary.Remove("mp3"))
+				Console.WriteLine("mp3 키 값에 해당하는 데이터를 지움");
+			else
+				Console.WriteLine("mp3 키 값에 해당하는 데이터를 못지움");
+
+			if (dictionary.ContainsKey("mp3"))
+				Console.WriteLine("mp3 키 값의 데이터가 있음");
+			else
+				Console.WriteLine("mp3 키 값의 데이터가 없음");
+
+			string output;
+			if (dictionary.TryGetValue("bmp", out output))
+				Console.WriteLine(output);
+			else
+				Console.WriteLine("bmp 키 값의 데이터가 없음");
+		}
+
+		static void Main(string[] args)
+		{
+			DataStructure.HashTable<string, string> hashTable = new DataStructure.HashTable<string, string>();
+
+			hashTable.Add("txt", "텍스트 파일");
+			hashTable.Add("bmp", "이미지 파일");
+			hashTable.Add("mp3", "오디오 파일");
+
+			Console.WriteLine(hashTable["txt"]);       // 키값은 인덱서를 통해 접근
+
+			if (hashTable.ContainsKey("mp3"))
+				Console.WriteLine("mp3 키 값의 데이터가 있음");
+			else
+				Console.WriteLine("mp3 키 값의 데이터가 없음");
+
+			if (hashTable.Remove("mp3"))
+				Console.WriteLine("mp3 키 값에 해당하는 데이터를 지움");
+			else
+				Console.WriteLine("mp3 키 값에 해당하는 데이터를 못지움");
+
+			if (hashTable.ContainsKey("mp3"))
+				Console.WriteLine("mp3 키 값의 데이터가 있음");
+			else
+				Console.WriteLine("mp3 키 값의 데이터가 없음");
+
+			string output;
+			if (hashTable.TryGetValue("bmp", out output))
+				Console.WriteLine(output);
+			else
+				Console.WriteLine("bmp 키 값의 데이터가 없음");
+		}
+	}
+}
