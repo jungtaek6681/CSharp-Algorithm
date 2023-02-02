cd726304fb1ce7416f8842ab7869fdc3a3b9dcdc
jungtaek kim <kgajungtaek@gmail.com>
2023-02-02 20:20:45 +0900

    feat : Sorting
    선택정렬 : 데이터 중 가장 작은 값부터 하나씩 선택하여 정렬
    삽입정렬 : 데이터를 하나씩 꺼내어 정렬된 자료 중 적합한 위치에 삽입하여 정렬
    버블정렬 : 서로 인접한 데이터를 비교하여 정렬
    합병정렬 : 데이터를 2분할하여 정렬 후 합병
    퀵정렬 : 하나의 피벗을 기준으로 작은값과 큰값을 2분할하여 정렬

---
+++ 10. Sorting/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10._Sorting
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Random random = new Random();
+
+			List<int> selectionList = new List<int>();
+			List<int> insertionList = new List<int>();
+			List<int> bubbleList = new List<int>();
+			List<int> mergeList = new List<int>();
+			List<int> quickList = new List<int>();
+
+			Console.WriteLine("랜덤 데이터 : ");
+			for (int i = 0; i < 20; i++)
+			{
+				int rand = random.Next() % 100;
+				Console.Write(string.Format("{0} ", rand));
+
+				selectionList.Add(rand);
+				insertionList.Add(rand);
+				bubbleList.Add(rand);
+				mergeList.Add(rand);
+				quickList.Add(rand);
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("선택 정렬 결과 : ");
+			Sort.SelectionSort(selectionList);
+			foreach (int i in selectionList)
+			{
+				Console.Write(string.Format("{0} ", i));
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("삽입 정렬 결과 : ");
+			Sort.InsertionSort(insertionList);
+			foreach (int i in selectionList)
+			{
+				Console.Write(string.Format("{0} ", i));
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("버블 정렬 결과 : ");
+			Sort.BubbleSort(bubbleList);
+			foreach (int i in selectionList)
+			{
+				Console.Write(string.Format("{0} ", i));
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("합병 정렬 결과 : ");
+			Sort.MergeSort(mergeList, 0, mergeList.Count - 1);
+			foreach (int i in selectionList)
+			{
+				Console.Write(string.Format("{0} ", i));
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("퀵 정렬 결과 : ");
+			Sort.QuickSort(quickList, 0, quickList.Count - 1);
+			foreach (int i in selectionList)
+			{
+				Console.Write(string.Format("{0} ", i));
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ 10. Sorting/Sort.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._Sorting
+{
+	internal class Sort
+	{
+		/******************************************************
+		 * 선형 정렬
+		 * 
+		 * 1개의 요소를 재위치시키기 위해 전체를 확인하는 정렬
+		 * n개의 요소를 재위치시키기 위해 n개를 확인하는 정렬
+		 * 시간복잡도 : O(N^2)
+		 ******************************************************/
+
+		// <선택정렬>
+		// 데이터 중 가장 작은 값부터 하나씩 선택하여 정렬
+		public static void SelectionSort(IList<int> list)
+		{
+			for (int i = 0; i < list.Count; i++)
+			{
+				int minIndex = i;
+				for (int j = i + 1; j < list.Count; j++)
+				{
+					if (list[j] < list[minIndex])
+						minIndex = j;
+				}
+				Swap(list, i, minIndex);
+			}
+		}
+
+		// <삽입정렬>
+		// 데이터를 하나씩 꺼내어 정렬된 자료 중 적합한 위치에 삽입하여 정렬
+		public static void InsertionSort(IList<int> list)
+		{
+			for (int i = 1; i < list.Count; i++)
+			{
+				int key = list[i];
+				int j;
+				for (j = i - 1; j >= 0 && key < list[j]; j--)
+				{
+					list[j + 1] = list[j];
+				}
+				list[j + 1] = key;
+			}
+		}
+
+		// <버블정렬>
+		// 서로 인접한 데이터를 비교하여 정렬
+		public static void BubbleSort(IList<int> list)
+		{
+			for (int i = 0; i < list.Count; i++)
+			{
+				for (int j = 1; j < list.Count; j++)
+				{
+					if (list[j - 1] > list[j])
+						Swap(list, j - 1, j);
+				}
+			}
+		}
+
+		/******************************************************
+		 * 분할정복 정렬
+		 * 
+		 * 1개의 요소를 재위치시키기 위해 전체의 1/2를 확인하는 정렬
+		 * n개의 요소를 재위치시키기 위해 n/2개를 확인하는 정렬
+		 * 시간복잡도 : O(NlogN)
+		 ******************************************************/
+
+		// <합병정렬>
+		// 데이터를 2분할하여 정렬 후 합병
+		public static void MergeSort(IList<int> list, int left, int right)
+		{
+			if (left == right) return;
+
+			int mid = (left + right) / 2;
+			MergeSort(list, left, mid);
+			MergeSort(list, mid + 1, right);
+			Merge(list, left, mid, right);
+		}
+
+		public static void Merge(IList<int> list, int left, int mid, int right)
+		{
+			List<int> sortedList = new List<int>();
+			int leftIndex = left;
+			int rightIndex = mid + 1;
+
+			// 분할 정렬된 List를 병합
+			while (leftIndex <= mid && rightIndex <= right)
+			{
+				if (list[leftIndex] < list[rightIndex])
+					sortedList.Add(list[leftIndex++]);
+				else
+					sortedList.Add(list[rightIndex++]);
+			}
+
+			if (leftIndex > mid)    // 왼쪽 List가 먼저 소진 됐을 경우
+			{
+				for (int i = rightIndex; i <= right; i++)
+					sortedList.Add(list[i]);
+			}
+			else  // 오른쪽 List가 먼저 소진 됐을 경우
+			{
+				for (int i = leftIndex; i <= mid; i++)
+					sortedList.Add(list[i]);
+			}
+
+			// 정렬된 sortedList를 list로 재복사
+			for (int i = left; i <= right; i++)
+			{
+				list[i] = sortedList[i - left];
+			}
+		}
+
+		// <퀵정렬>
+		// 하나의 피벗을 기준으로 작은값과 큰값을 2분할하여 정렬
+		public static void QuickSort(IList<int> list, int start, int end)
+		{
+			if (start >= end) return;
+
+			int pivotIndex = start;
+			int leftIndex = pivotIndex + 1;
+			int rightIndex = end;
+
+			while (leftIndex <= rightIndex) // 엇갈릴때까지 반복
+			{
+				// pivot보다 큰 값을 만날때까지
+				while (list[leftIndex] <= list[pivotIndex] && leftIndex < end)
+					leftIndex++;
+				while (list[rightIndex] >= list[pivotIndex] && rightIndex > start)
+					rightIndex--;
+
+				if (leftIndex < rightIndex)     // 엇갈리지 않았다면
+					Swap(list, leftIndex, rightIndex);
+				else	// 엇갈렸다면
+					Swap(list, pivotIndex, rightIndex);
+			}
+
+			QuickSort(list, start, rightIndex - 1);
+			QuickSort(list, rightIndex + 1, end);
+		}
+
+
+		private static void Swap(IList<int> list, int left, int right)
+		{
+			int temp = list[left];
+			list[left] = list[right];
+			list[right] = temp;
+		}
+	}
+}
