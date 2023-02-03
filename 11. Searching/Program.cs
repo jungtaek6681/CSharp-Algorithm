12e1de0c2c80103cfbf79177bc98a55981b009a4
jungtaek kim <kgajungtaek@gmail.com>
2023-02-03 19:56:24 +0900

    feat : Searching
    DFS(깊이우선탐색) : 그래프의 분기를 만났을 때 최대한 깊이 내려간 뒤, 더이상 깊이 갈 곳이 없을 경우 다음분기를 탐색
    BFS(너비우선탐색) : 그래프의 분기를 만났을 때 모든 분기를 저장한 뒤, 저장한 분기를 하나씩 탐색

---
+++ 11. Searching/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _11._Searching
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			bool[,] graph = new bool[8, 8]
+			{
+				{ false,  true, false, false, false, false, false, false },
+				{  true, false,  true, false, false,  true, false, false },
+				{ false,  true, false, false,  true,  true, false, false },
+				{ false, false, false, false, false,  true, false, false },
+				{ false, false,  true, false, false, false,  true,  true },
+				{ false,  true,  true,  true, false, false, false, false },
+				{ false, false, false, false,  true, false, false, false },
+				{ false, false, false, false,  true, false, false, false },
+			};
+
+			// DFS 탐색
+			bool[] dfsVisited;
+			int[] dfsPath;
+			Search.DFS(in graph, 0, out dfsVisited, out dfsPath);
+			Console.WriteLine("<DFS>");
+			PrintResult(dfsVisited, dfsPath);
+			Console.WriteLine();
+
+			// BFS 탐색
+			bool[] bfsVisited;
+			int[] bfsPath;
+			Search.BFS(in graph, 0, out bfsVisited, out bfsPath);
+			Console.WriteLine("<BFS>");
+			PrintResult(bfsVisited, bfsPath);
+			Console.WriteLine();
+		}
+
+		private static void PrintResult(bool[] dfsVisited, int[] dfsPath)
+		{
+			Console.Write("Vertex");
+			Console.Write("\t");
+			Console.Write("Visit");
+			Console.Write("\t");
+			Console.WriteLine("Path");
+
+			for (int i = 0; i < dfsVisited.Length; i++)
+			{
+				Console.Write(i);
+				Console.Write("\t");
+				Console.Write(dfsVisited[i]);
+				Console.Write("\t");
+				Console.WriteLine(dfsPath[i]);
+			}
+		}
+	}
+}
---
+++ 11. Searching/Search.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._Searching
+{
+	internal class Search
+	{
+		// <깊이 우선 탐색 (Depth-First Search)>
+		// 그래프의 분기를 만났을 때 최대한 깊이 내려간 뒤,
+		// 더이상 깊이 갈 곳이 없을 경우 다음 분기를 탐색
+		// 분할정복을 통해 구현
+		public static void DFS(in bool[,] graph, int start, out bool[] visited, out int[] parents)
+		{
+			visited = new bool[graph.GetLength(0)];
+			parents = new int[graph.GetLength(0)];
+
+			for (int i = 0; i < graph.GetLength(0); i++)
+			{
+				visited[i] = false;
+				parents[i] = -1;
+			}
+
+			SearchNode(graph, start, visited, parents);
+		}
+
+		private static void SearchNode(in bool[,] graph, int start, bool[] visited, int[] parents)
+		{
+			visited[start] = true;
+			for (int i = 0; i < graph.GetLength(0); i++)
+			{
+				if (graph[start, i] &&		// 연결되어 있는 정점이며,
+					!visited[i])			// 방문한적 없는 정점
+				{
+					parents[i] = start;
+					SearchNode(graph, i, visited, parents);
+				}
+			}
+		}
+
+		// <너비 우선 탐색 (Breadth-First Search)>
+		// 그래프의 분기를 만났을 때 모든 분기를 저장한 뒤,
+		// 저장한 분기를 하나씩 탐색
+		// 동적계획법을 통해 구현
+		public static void BFS(in bool[,] graph, int start, out bool[] visited, out int[] parents)
+		{
+			visited = new bool[graph.GetLength(0)];
+			parents = new int[graph.GetLength(0)];
+
+			for (int i = 0; i < graph.GetLength(0); i++)
+			{
+				visited[i] = false;
+				parents[i] = -1;
+			}
+
+			Queue<int> bfsQueue = new Queue<int>();
+
+			bfsQueue.Enqueue(start);
+			while (bfsQueue.Count > 0)
+			{
+				int next = bfsQueue.Dequeue();
+				visited[next] = true;
+
+				for (int i = 0; i < graph.GetLength(0); i++)
+				{ 
+					if (graph[next, i] &&		// 연결되어 있는 정점이며,
+						!visited[i])			// 방문한적 없는 정점
+					{
+						parents[i] = next;
+						bfsQueue.Enqueue(i);
+					}
+				}
+			}
+		}
+	}
+}
