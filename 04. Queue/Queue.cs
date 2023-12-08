27de868ceb30c62bf7531e12ce8414f841d43178
jungtaek kim <kgajungtaek@gmail.com>
2023-12-09 02:27:43 +0900

    feat : Queue
    Queue : 선입선출(FIFO), 후입후출(LILO) 방식의 자료구조

---
+++ 04. Queue/Queue.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class Queue<T>
+    {
+        private const int DefaultCapacity = 4;
+
+        private T[] array;
+        private int head;
+        private int tail;
+
+        public Queue()
+        {
+            array = new T[DefaultCapacity];
+            head = 0;
+            tail = 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                if (head <= tail)
+                {
+                    return tail - head;
+                }
+                else
+                {
+                    return tail + (array.Length - head);
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            array = new T[DefaultCapacity];
+            head = 0;
+            tail = 0;
+        }
+
+        public void Enqueue(T item)
+        {
+            if (IsFull())
+            {
+                Grow();
+            }
+
+            array[tail] = item;
+            MoveNext(ref tail);
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException();
+
+            T result = array[head];
+            MoveNext(ref head);
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException();
+
+            return array[head];
+        }
+
+        private void Grow()
+        {
+            int newCapacity = array.Length * 2;
+            T[] newArray = new T[newCapacity];
+            if (head < tail)
+            {
+                Array.Copy(array, head, newArray, 0, tail);
+            }
+            else
+            {
+                Array.Copy(array, head, newArray, 0, array.Length - head);
+                Array.Copy(array, 0, newArray, array.Length - head, tail);
+            }
+
+            array = newArray;
+            tail = Count;
+            head = 0;
+        }
+
+        private bool IsFull()
+        {
+            if (head > tail)
+            {
+                return head == tail + 1;
+            }
+            else
+            {
+                return head == 0 && tail == array.Length - 1;
+            }
+        }
+
+        private bool IsEmpty()
+        {
+            return head == tail;
+        }
+
+        private void MoveNext(ref int index)
+        {
+            index = (index + 1) % array.Length;
+        }
+    }
+}
+
