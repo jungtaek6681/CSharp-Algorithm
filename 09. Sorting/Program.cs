3d573704001338104182641d46e6848dc415e80e
jungtaek kim <kgajungtaek@gmail.com>
2024-01-11 00:07:34 +0900

    feat : Sorting
    선택정렬 : 데이터 중 가장 작은 값부터 하나씩 선택하여 정렬
    삽입정렬 : 데이터를 하나씩 꺼내어 정렬된 자료 중 적합한 위치에 삽입하여 정렬
    버블정렬 : 서로 인접한 데이터를 비교하여 정렬
    합병정렬 : 데이터를 2분할하여 정렬 후 합병
    퀵정렬 : 하나의 피벗을 기준으로 작은값과 큰값을 2분할하여 정렬
    힙정렬 : 힙을 이용하여 우선순위가 높은 순서대로 정렬

---
+++ 09. Sorting/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace _09._Sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int count = 20;
+
+            List<int> selectionList = new List<int>(count);
+            List<int> insertionList = new List<int>(count);
+            List<int> bubbleList = new List<int>(count);
+            List<int> mergeList = new List<int>(count);
+            List<int> quickList = new List<int>(count);
+            List<int> heapList = new List<int>(count);
+            List<int> introList = new List<int>(count);
+
+            Console.WriteLine("랜덤 데이터 : ");
+            for (int i = 0; i < count; i++)
+            {
+                int rand = random.Next() % 100;
+                Console.Write($"{rand,3}");
+
+                selectionList.Add(rand);
+                insertionList.Add(rand);
+                bubbleList.Add(rand);
+                heapList.Add(rand);
+                mergeList.Add(rand);
+                quickList.Add(rand);
+                introList.Add(rand);
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("선택 정렬 결과 : ");
+            Sorting.SelectionSort(selectionList);
+            foreach (int i in selectionList)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("삽입 정렬 결과 : ");
+            Sorting.InsertionSort(insertionList);
+            foreach (int i in insertionList)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("버블 정렬 결과 : ");
+            Sorting.BubbleSort(bubbleList);
+            foreach (int i in bubbleList)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("합병 정렬 결과 : ");
+            Sorting.MergeSort(mergeList);
+            foreach (int i in mergeList)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("퀵 정렬 결과 : ");
+            Sorting.QuickSort(quickList);
+            foreach (int i in quickList)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("힙 정렬 결과 : ");
+            Sorting.HeapSort(heapList);
+            foreach (int i in heapList)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("인트로 정렬 결과 : ");
+            introList.Sort();
+            foreach (int i in introList)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
