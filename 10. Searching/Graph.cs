6f9ada80997ea4a76a4d45d9cb0327049a77a63e
jungtaek kim <kgajungtaek@gmail.com>
2024-01-11 00:21:08 +0900

    feat : Searching
    순차탐색 ; 자료구조에서 순차적으로 찾고자 하는 데이터를 탐색
    이진탐색 : 정렬이 되어있는 자료구조에서 2분할을 통해 데이터를 탐색
    DFS(깊이우선탐색) : 그래프의 분기들을 만났을 때 한 분기를 최대한 깊이 탐색한 뒤, 분기의 탐색을 마쳤을 때 다음분기를 탐색
    BFS(너비우선탐색) : 그래프의 분기들을 만났을 때 모든 분기를 탐색한 뒤, 다음 깊이의 분기들을 탐색

---
+++ 10. Searching/Graph.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._Searching
+{
+    internal class Graph
+    {
+        /************************************************************************
+         * 그래프 (Graph)
+         * 
+         * 정점의 모음과 이 정점을 잇는 간선의 모음의 결합
+         * 한 노드에서 출발하여 다시 자기 자신의 노드로 돌아오는 순환구조를 가짐
+         * 간선의 방향성에 따라 단방향 그래프, 양방향 그래프가 있음
+         * 간선의 가중치에 따라   연결 그래프, 가중치 그래프가 있음
+         ************************************************************************/
+
+        // <인접행렬 그래프>
+        // 그래프 내의 각 정점의 인접 관계를 나타내는 행렬
+        // 2차원 배열을 [출발정점, 도착정점] 으로 표현
+        // 장점 : 인접여부 접근이 빠름
+        // 단점 : 메모리 사용량이 많음
+
+        // 예시 - 양방향 연결 그래프
+        bool[,] matrixGraph1 = new bool[5, 5]
+        {
+            { false, false, false, false,  true },
+            { false, false,  true, false, false },
+            { false,  true, false,  true, false },
+            { false, false,  true, false,  true },
+            {  true, false, false,  true, false },
+        };
+
+        const int INF = int.MaxValue;
+
+        // 예시 - 단방향 가중치 그래프 (단절은 최대값으로 표현)
+        int[,] matrixGraph2 = new int[5, 5]
+        {
+            {   0, 132, INF, INF,  16 },
+            {  12,   0, INF, INF, INF },
+            { INF,  38,   0, INF, INF },
+            { INF,  12, INF,   0,  54 },
+            { INF, INF, INF, INF,   0 },
+        };
+
+
+        // <인접리스트 그래프>
+        // 그래프 내의 각 정점의 인접 관계를 표현하는 리스트
+        // 인접한 간선만을 리스트에 추가하여 관리
+        // 장점 : 메모리 사용량이 적음
+        // 단점 : 인접여부를 확인하기 위해 리스트 탐색이 필요
+
+        // 예시
+        List<int>[] listGraph1;                 // 연결 그래프
+        List<(int, int)>[] listGraph2;          // 가중치 그래프
+        public void CreateGraph()
+        {
+            listGraph1 = new List<int>[5];
+
+            listGraph1[0].Add(1);
+            listGraph1[1].Add(0);
+            listGraph1[1].Add(3);
+            listGraph1[2].Add(0);
+            listGraph1[2].Add(1);
+            listGraph1[2].Add(4);
+            listGraph1[3].Add(1);
+            listGraph1[4].Add(3);
+        }
+    }
+}
---
+++ 10. Searching/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace _10._Searching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 순차탐색
+            int index1;
+            int index2;
+            int[] array = { 1, 3, 5, 7, 9, 8, 6, 4, 2, 0 };
+            Console.Write("배열 : ");
+            foreach (int i in array)
+            {
+                Console.Write($"{i,2}");
+            }
+            Console.WriteLine();
+
+            index1 = Array.IndexOf(array, 2);
+            index2 = Searching.SequentialSearch(array, 2);
+            Console.WriteLine($"순차탐색 결과 위치 : {index1}");
+            Console.WriteLine($"구현한 순차탐색 결과 위치 : {index2}");
+            Console.WriteLine();
+
+
+            // 이진탐색
+            index1 = Array.BinarySearch(array, 2);
+            index2 = Searching.BinarySearch(array, 2);
+            Console.WriteLine("정렬 전 결과");
+            Console.WriteLine($"이진탐색 결과 위치 : {index1}");
+            Console.WriteLine($"구현한 이진탐색 결과 위치 : {index2}");
+            Console.WriteLine();
+
+            Array.Sort(array);  // 이진탐색의 경우 우선 정렬이 필요
+            Console.Write("정렬된 배열 : ");
+            foreach (int i in array)
+            {
+                Console.Write($"{i,2}");
+            }
+            Console.WriteLine();
+
+            index1 = Array.BinarySearch(array, 2);
+            index2 = Searching.BinarySearch(array, 2);
+            Console.WriteLine("정렬 후 결과");
+            Console.WriteLine($"이진탐색 결과 위치 : {index1}");
+            Console.WriteLine($"구현한 이진탐색 결과 위치 : {index2}");
+            Console.WriteLine();
+
+
+            bool[,] graph = new bool[8, 8]
+            {
+                { false,  true, false, false, false, false, false, false },
+                {  true, false,  true, false, false,  true, false, false },
+                { false,  true, false, false,  true,  true, false, false },
+                { false, false, false, false, false,  true, false, false },
+                { false, false,  true, false, false, false,  true,  true },
+                { false,  true,  true,  true, false, false, false, false },
+                { false, false, false, false,  true, false, false, false },
+                { false, false, false, false,  true, false, false, false },
+            };
+
+
+            // DFS 탐색
+            Console.WriteLine("<DFS>");
+            Searching.DFS(graph, 0, out bool[] dfsVisited, out int[] dfsParents);
+            PrintGraphSearch(dfsVisited, dfsParents);
+            Console.WriteLine();
+
+
+            // BFS 탐색
+            Console.WriteLine("<BFS>");
+            Searching.BFS(graph, 0, out bool[] bfsVisited, out int[] bfsParents);
+            PrintGraphSearch(bfsVisited, bfsParents);
+            Console.WriteLine();
+        }
+
+        private static void PrintGraphSearch(bool[] visited, int[] parents)
+        {
+            Console.WriteLine($"{"Vertex",8}{"Visit",8}{"Parent",8}");
+
+            for (int i = 0; i < visited.Length; i++)
+            {
+                Console.WriteLine($"{i,8}{visited[i],8}{parents[i],8}");
+            }
+        }
+    }
+}
