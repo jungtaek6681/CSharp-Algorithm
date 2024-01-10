7ca4faed10d015e9b3a391371659f399a6bee7d3
jungtaek kim <kgajungtaek@gmail.com>
2024-01-10 23:50:00 +0900

    docs : 알고리즘 설계기법
    탐욕 알고리즘 : 문제를 직면한 당시에 당장 최적인 답을 선택하는 과정을 반복
    분할정복 : 큰 문제를 작은 문제로 나눠서 푸는 하향식 접근 방식
    동적계획법 : 작은문제의 해답을 큰문제의 해답의 부분으로 이용하는 상향식 접근 방식
    백트래킹 : 모든 경우의 수를 전부 고려하는 알고리즘

---
+++ 08. DesignTechnique/Backtracking.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._DesignTechnique
+{
+    internal class Backtracking
+    {
+        /*********************************************************************
+         * 백트래킹 (Backtracking)
+         * 
+         * 모든 경우의 수를 전부 고려하는 알고리즘
+         * 후보해를 검증 도중 해가 아닌경우 되돌아가서 다시 해를 찾아가는 기법
+         **********************************************************************/
+
+        // 예시 - 순열
+        List<int> list = new List<int>();
+        void Permutation(int n, int r, int count = 0)
+        {
+            if (count == r)
+            {
+                Console.WriteLine(string.Join(' ', list));
+                return;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (list.Contains(i))
+                {
+                    continue;
+                }
+
+                list.Add(i);
+                Permutation(n, r, i + 1);
+                list.RemoveAt(list.Count - 1);
+            }
+        }
+    }
+}
---
+++ 08. DesignTechnique/DynamicProgramming.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._DesignTechnique
+{
+    internal class DynamicProgramming
+    {
+        /**********************************************************************************
+         * 동적계획법 (Dynamic Programming)
+         * 
+         * 작은문제의 해답을 큰문제의 해답의 부분으로 이용하는 상향식 접근 방식
+         * 주어진 문제를 해결하기 위해 부분 문제에 대한 답을 계속적으로 활용해 나가는 기법
+         ***********************************************************************************/
+
+        // 예시 - 피보나치 수열
+        int Fibonachi(int x)
+        {
+            int[] fibonachi = new int[x + 1];
+            fibonachi[1] = 1;
+            fibonachi[2] = 1;
+
+            for (int i = 3; i <= x; i++)
+            {
+                fibonachi[i] = fibonachi[i - 1] + fibonachi[i - 2];
+            }
+
+            return fibonachi[x];
+        }
+    }
+}
