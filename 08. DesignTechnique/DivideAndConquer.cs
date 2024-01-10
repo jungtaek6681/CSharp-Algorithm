7254bdd3f181350dfb49423733dfec0b3b61f0f9
jungtaek kim <kgajungtaek@gmail.com>
2024-01-10 23:50:00 +0900

    docs : 알고리즘 설계기법
    탐욕 알고리즘 : 문제를 직면한 당시에 당장 최적인 답을 선택하는 과정을 반복
    분할정복 : 큰 문제를 작은 문제로 나눠서 푸는 하향식 접근 방식
    동적계획법 : 작은문제의 해답을 큰문제의 해답의 부분으로 이용하는 상향식 접근 방식
    백트래킹 : 모든 경우의 수를 전부 고려하는 알고리즘

---
+++ 08. DesignTechnique/DivideAndConquer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._DesignTechnique
+{
+    internal class DivideAndConquer
+    {
+        /*************************************************************************
+         * 분할정복 (Divide and Conquer)
+         * 
+         * 큰 문제를 작은 문제로 나눠서 푸는 하향식 접근 방식
+         * 분할을 통해서 해결하기 쉬운 작은 문제로 나눈 후 정복한 후 병합하는 과정
+         **************************************************************************/
+
+        // 예시 - 거듭 제곱
+        int Pow(int x, int n)
+        {
+            if (n == 1)
+            {
+                return x;
+            }
+
+            int halfPow = Pow(x, n / 2);
+            if (n % 2 == 0)
+            {
+                return halfPow * halfPow;
+            }
+            else
+            {
+                return halfPow * halfPow * x;
+            }
+        }
+    }
+}
---
+++ 08. DesignTechnique/Greedy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._DesignTechnique
+{
+    internal class Greedy
+    {
+        /************************************************************
+         * 탐욕 알고리즘 (Greedy Algorithm)
+         * 
+         * 문제를 해결하는데 사용되는 근시안(짧은시야)적 방법
+         * 문제를 직면한 당시에 당장 최적인 답을 선택하는 과정을 반복
+         * 단, 반드시 최적의 해를 구해준다는 보장이 없음
+         *************************************************************/
+
+        // 예시 - 자판기 거스름돈
+        void CoinMachine(int money)
+        {
+            int[] coinType = { 500, 100, 50, 10, 5, 1 };
+
+            foreach (int coin in coinType)
+            {
+                while (money <= coin)
+                {
+                    Console.WriteLine($"{coin} 코인 배출");
+                    money -= coin;
+                }
+            }
+        }
+    }
+}
